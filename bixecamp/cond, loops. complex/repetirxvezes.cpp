e4e51ddd0c91c9fe8a3f64f991c8ab4e53bdaf8a
panyfmc <paulavanessa010@gmail.com>
2025-12-27 13:09:53 -0300

    feat: repetir x vezes question

---
+++ bixecamp/cond, loops. complex/repetirxvezes.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int X; cin >> X;
+    // int i = 0;
+    // while (i < X) 
+    // {
+    //     cout << "NepsAcademy eh Sucesso" << endl;
+    //     i++;
+    // }
+    for(int i; i<X; i++) {
+        cout << "NepsAcademy eh Sucesso" << endl;
+    }
+    
+
+
+    return 0;
+}
