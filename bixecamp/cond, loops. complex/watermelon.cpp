7b33e2cd4251cb5e9090526a08da867078fd23f2
panyfmc <paulavanessa010@gmail.com>
2025-12-29 15:43:52 -0300

    feat: watermelon question

---
+++ bixecamp/cond, loops. complex/watermelon.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    int p; cin >> p;
+    if(p%2==0 && p>=4) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+    
+    return 0;
+}
