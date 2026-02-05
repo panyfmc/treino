c1f1ca402448f019a809071e94872a3091cea244
pany <153379216+panyfmc@users.noreply.github.com>
2026-02-05 16:30:40 -0300

    feat: divisor finding program in commond.cpp
    Implement a program to find the largest divisor that appears at least twice in the input numbers.

---
+++ teoria dos numeros/commond.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int MAXX = 1e6;
+#define _ ios::sync_with_stdio(false); cin.tie(nullptr);
+
+int main() {
+    _
+
+    int n;
+    cin >> n;
+    
+    vector<int> freq(MAXX + 1, 0);
+
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        freq[x]++;
+    }
+
+    // Testa divisores do maior para o menor
+    for (int d = MAXX; d >= 1; d--) {
+        int cnt = 0;
+        for (int multiple = d; multiple <= MAXX; multiple += d) {
+            cnt += freq[multiple];
+            if (cnt >= 2) {
+                cout << d << endl;
+                return 0;
+            }
+        }
+    }
+
+    cout << 1 << endl;
+    return 0;
+}
