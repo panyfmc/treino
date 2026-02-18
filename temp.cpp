c058c66d5196a88273f43fd25b6d2a940fa8a631
pany <153379216+panyfmc@users.noreply.github.com>
2026-02-18 11:28:34 -0300

    Refactor macros and type definitions

--- temp.cpp
+++ temp.cpp
@@ -1,12 +1,12 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-
 #define _ ios_base::sync_with_stdio(false); cin.tie(nullptr);
 #define f first 
 #define s second
-
-typedef long long ll;
+#define endl '\n'
+#define ll long long
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
 const int INF = 0x3f3f3f3f;
 const ll LINF = 0x3f3f3f3f3f3f3f3fll;
 
