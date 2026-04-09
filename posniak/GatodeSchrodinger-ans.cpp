4464976da0facd1e39b65fa7782b0196030f4b3b
panyfmc <153379216+panyfmc@users.noreply.github.com>
2026-04-09 15:38:52 -0300

    Add files via upload

---
+++ posniak/GatodeSchrodinger-ans.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define _ ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+int main() {
+    _ 
+    string noticia;
+	cin >> noticia;
+	if(noticia == "miou") {
+	    cout << "VIVO" << endl;
+	} else cout << "MORTO" << endl;
+    return 0;
+
+}
+ 
