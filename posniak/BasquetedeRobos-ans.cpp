278732bfa60057f94022d1c19188a574c8e1d0b3
panyfmc <153379216+panyfmc@users.noreply.github.com>
2026-04-15 22:39:29 -0300

    calculando pontos com base na area

---
+++ posniak/BasquetedeRobos-ans.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define _ ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define endl '\n'
+
+int main() {
+	_ 
+	int d; cin >> d;
+	if(d <= 800) {
+	    cout << "1" << endl;
+	} else if(d <= 1400) {
+	    cout << "2" << endl;
+	} else {
+	    cout << "3" << endl;
+	}
+    return 0;
+}
