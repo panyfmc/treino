d4ed4a5455eb43193f2b55524f71ceb0c5615aa8
panyfmc <153379216+panyfmc@users.noreply.github.com>
2026-04-20 13:37:27 -0300

    Add vacina.cpp for input-based output logic

---
+++ posniak/vacina.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define _ ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define endl '\n'
+//akiro akira
+int main() {
+	_ 
+    int a, b; cin >> a >> b;
+    if(a < 8 || b < 8) cout << "bom jesus" << endl;
+    else if(a >= 8 && b >= 8) cout << "bom jesus" << endl;
+    else cout << "cruzeiro" << endl;
+	
+	return 0;
+}
