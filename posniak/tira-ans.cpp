79fe57ad106793033359a0a6a07a32a094f7c6b2
panyfmc <153379216+panyfmc@users.noreply.github.com>
2026-04-20 13:17:07 -0300

    fix: se y estivesse fora não imprimia nada antes

--- posniak/tira-ans.cpp
+++ posniak/tira-ans.cpp
@@ -6,10 +6,8 @@ using namespace std;
 int main() {
 	_ 
     int x, y; cin >> x >> y;
-    if(x >= 0 && x <= 432) {
-        if(y >= 0 && y <= 468) {
-            cout << "dentro" << endl;
-        }
+    if(x >= 0 && x <= 432 && y >= 0 && y <= 468) {
+        cout << "dentro" << endl;
     } else cout << "fora" << endl;
 	
 	return 0;
